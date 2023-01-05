5ed9bd44509ee5e24c1ceb18f8d6243d8a6b5ae0
Kwabena Nana Acheampong <achiekobby17@gmail.com>
2023-01-05 11:49:58 +0000

    Workout model creation

--- App/Controllers/WorkoutController.js
+++ App/Controllers/WorkoutController.js
@@ -1,30 +1,40 @@
+//* Import Workout model
+const Workout = require("../Models/Workout");
+
 //* List of CRUD operations implementation of the workout application
 
-const getAllWorkouts = (req,res)=>{
-  res.send("all workouts")
-}
+const getAllWorkouts = (req, res) => {
+  res.send("all workouts");
+};
 
-const getWorkout = (req,res)=>{
-  res.send("Get single workouts")
-}
+const getWorkout = (req, res) => {
+  res.send("Get single workouts");
+};
 
-const createWorkout = (req,res)=>{
-  res.send("create a workouts")
-}
+const createWorkout = async (req, res) => {
+  const { title, load, reps } = req.body;
+  try {
+    let new_workout = await Workout.create({ title, load, reps });
+    if (new_workout) {
+      res.status(200).json(new_workout);
+    }
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
 
-const updateWorkout = (req,res)=>{
-  res.send("update a workouts")
-}
+const updateWorkout = (req, res) => {
+  res.send("update a workouts");
+};
 
-const deleteWorkout = (req,res)=>{
-  res.send("delete a workouts")
-}
-
+const deleteWorkout = (req, res) => {
+  res.send("delete a workouts");
+};
 
 module.exports = {
   getAllWorkouts,
   getWorkout,
   createWorkout,
   updateWorkout,
-  deleteWorkout
-}
+  deleteWorkout,
+};
