c97ea19a3f0dde07c80acfa4e33e6f132ba472ba
Kwabena Nana Acheampong <achiekobby17@gmail.com>
2023-01-09 08:38:54 +0000

    handling the errors in node

--- App/Controllers/WorkoutController.js
+++ App/Controllers/WorkoutController.js
@@ -39,6 +39,20 @@ const getWorkout = async (req, res) => {
 const createWorkout = async (req, res) => {
   const { title, load, reps } = req.body;
   try {
+    let emptyFields = []
+    if(!title){
+      emptyFields.push('title')
+    }
+    if(!load){
+      emptyFields.push('load')
+    }
+    if(!reps){
+      emptyFields.push('reps')
+    }
+
+    if(emptyFields.length >0){
+      return res.status(400).json({error:'Please fill in all the fields', emptyFields})
+    }
     let new_workout = await Workout.create({ title, load, reps });
     if (new_workout) {
       res.status(200).json(new_workout);
@@ -75,13 +89,14 @@ const updateWorkout = async (req, res) => {
 
 const deleteWorkout = async (req, res) => {
   const id = req.params.id;
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    res.status(422).json({ error: "The id passed is invalid" });
-  }
+
   try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(422).json({ error: "The id passed is invalid" });
+    }
     let workout = await Workout.findOneAndDelete({ _id: id });
     if (workout) {
-      res.status(200).json({
+      return res.status(200).json({
         status: "success",
         message: "successfully deleted an item from the workout",
         workout: workout,
@@ -91,7 +106,7 @@ const deleteWorkout = async (req, res) => {
       .status(404)
       .json({ status: "failed", message: "The workout was not found" });
   } catch (err) {
-    res.status(500).json({ error: err });
+    return res.status(500).json({ error: err.message });
   }
 };
 
