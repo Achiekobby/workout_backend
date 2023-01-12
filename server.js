8dfd06a3665c468e7ffb3b56c848737dd82e032b
Kwabena Nana Acheampong <achiekobby17@gmail.com>
2023-01-12 16:06:32 +0000

    backend middleware bug fix

--- server.js
+++ server.js
@@ -16,12 +16,13 @@ const workoutRoutes = require("./App/Routes/workouts.js");
 
 //* import the user routers
 const userRoutes = require('./App/Routes/user')
+//* using the user routes
+app.use("/api/user", userRoutes);
 
 //* using the workouts routes
 app.use("/api", workoutRoutes);
 
-//* using the user routes
-app.use("/api/user", userRoutes);
+
 
 const start = async () => {
   await dbConnect(process.env.MONGO_URI)
